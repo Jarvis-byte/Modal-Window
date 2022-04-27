115c9febdadf97b143abc8ffadcb292018f23244
Arka <arkamazumder0@gmail.com>
2022-04-27 22:29:09 +0530

    Adding keypress ESC for modal

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-//Refractoring
+//Refractoring code
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
@@ -22,3 +22,9 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
